49f61ad859f1a54d1aa27199a712296dbcfa0b91
Terence Anton Dela Fuente <td@circuit.rocks>
2019-03-11 00:05:37 +0800

    add assets

--- mi-amor-app/src/components/gallery/GalleryBoard.js
+++ mi-amor-app/src/components/gallery/GalleryBoard.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import Gallery from 'react-photo-gallery';
+import pic1 from '../../assets/1.jpeg';
+import pic2 from '../../assets/2.jpeg';
+import pic3 from '../../assets/3.jpeg';
+import pic4 from '../../assets/4.jpeg';
+import pic5 from '../../assets/5.jpeg';
+import pic6 from '../../assets/6.jpeg';
+import pic7 from '../../assets/7.jpeg';
+import pic8 from '../../assets/8.jpeg';
+import pic9 from '../../assets/9.jpeg';
+import pic10 from '../../assets/10.jpeg';
 
 const photos = [
-  { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 1, height: 1 },
-  { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
-  { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+  { src: pic1, width: 2, height: 2 },
+  { src: pic2, width: 1, height: 2 },
+  { src: pic3, width: 3, height: 4 },
+  { src: pic4, width: 3, height: 4 },
+  { src: pic5, width: 3, height: 4 },
+  { src: pic6, width: 1, height: 2 },
+  { src: pic7, width: 1, height: 2 },
+  { src: pic8, width: 3, height: 4 },
+  { src: pic9, width: 3, height: 4 },
+  { src: pic10, width: 3, height: 4 }
 ];
 
 class GalleryBoard extends Component {
